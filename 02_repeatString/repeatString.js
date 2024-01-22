fc4ee15266fa3837353aa80bcaf9e3f6507d9f5d
Dirk  Hubregtse <20535135+dirkhubregtse@users.noreply.github.com>
2024-01-22 20:04:26 +0100

    Execute exercice 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+let outputString = "";
 
+const repeatString = function(string, num) {
+
+    for (let i = 0; i < num; i++) {
+        outputString = outputString + string;
+    }
+    if (num < 0){  
+        return "ERROR";
+    } else {
+        return outputString;
+    }
+    
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(stringetje) {
+    //split
+    let splitStringetje =  stringetje.split("");
+    //reverse
+    let revStringetje = splitStringetje.reverse();
+    //join
+    let joinStringetje = revStringetje.join("");
+    
+    return joinStringetje;
 
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
