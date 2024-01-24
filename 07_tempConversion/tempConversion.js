03530d55d3a4794a59b65658bc0f463e4dd8fd52
Dirk  Hubregtse <20535135+dirkhubregtse@users.noreply.github.com>
2024-01-24 19:41:14 +0100

    Finnish exercice no 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  let cecius = Math.round((((value - 32) * 5/9))*10)/10;
+  return cecius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  let fahrenheit = Math.round((((value * 9/5) + 32))*10)/10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
